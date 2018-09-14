ae052e4ebe1a0b219ba701cce1adf9f9538cd1a7
Blues <40874591+ABJervis@users.noreply.github.com>
2018-09-13 23:28:17 -0400

    still not functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,22 @@
 //create computer guess choices
 var computerChoices = 'abcdefghijklmnopqrstuvwxyz'.split("");
 
-//variables to count wins, losses, etc.
-var wins = 0
-var losses = 0
-var guessSoFar = []
-var guessesNumber = 10
-
-//variables to hold references to places in the HTML
+//define variables
+var wins = 0;
+var losses = 0;
+var guessSoFar = [];
+var guessesNumber = 9;
 
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("loss-text");
-var guessSoFarText = document.getElementById("guessSoFar");
-var guessesNumberText = document.getElementById("guessesLeft");
+var guessesMade = document.getElementById("guessSoFar");
+var numberLeft = document.getElementById("guessesLeft");
 
-
-// Randomly chooses a choice from the options array = Computer's guess. This has to done before user begins to guess.
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 console.log(computerGuess);
+//define functions that will be used to run the game
 
+//need to set reset so the game can be re-played
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
@@ -27,21 +25,34 @@ document.onkeyup = function(event) {
     var userGuess = event.key;
     console.log(event)
 
-//if user guesses correctly
+//if user guesses correctly - alert is more for me than a part of the game
 
 if(userGuess === computerGuess){
     wins++;
-    alert("Congratulations!")
+    alert("Congratulations!") 
 }
 
-//if user guess incorrectly
+//if user guess incorrectly - alert is more for me than a part of the game
+
 
 if(userGuess !== computerGuess){
     guessesNumber --;
+    
     alert("Try again!");
-
 }
 
 
+if(guessesNumber < 1){
+    alert("Game Over")
+}
+
+winsText.textContent = "Wins: " + wins;
+lossesText.textContent = "Losses: " + losses;
+guessesMade.textContent = "Guesses so Far: " + userGuess;
+numberLeft.textContent = "Guesses Left" - guessesLeft;
+
 
 }
+
+//need to learn how to display results to the page. cannot figure out how to get them to show, 
+//there's return and push and textcontent, not sure how to go 
